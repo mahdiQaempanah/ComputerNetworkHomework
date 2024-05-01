634a8c72877239b715806dde6f85b29ffe9b6a6c
mahdi <mahdi13812002@gmail.com>
2024-05-01 19:27:32 +0330

    debug

--- HW2/load_balancer.py
+++ HW2/load_balancer.py
@@ -3,38 +3,51 @@ import threading
 import os
 from queue import Queue
 import re 
+from enum import Enum
 import time 
 
 config_path = './config'
 ip = "127.0.0.1"
-port = 65432
+port = 65430
+
+class RequestType(Enum):
+    CONNECTION_TEST = 'CONNECTION_TEST'
+
+class ResponeType(Enum):
+    OK = 'OK'
+    INVALID_REQUEST = 'INVALID_REQUEST'
+    
 
 class LoadBalancer:
-    def __init__(self, config_path, timeout=2., server_availability_check_period=1) -> None:
+
+    def __init__(self, ip, port, config_path, timeout=2., server_availability_check_period=5.) -> None:
+        self.ip = ip 
+        self.port = port 
         self.all_servers = self.extract_servers(config_path)
+        self.request_queue = Queue()
         self.timeout = timeout
         self.check_period = server_availability_check_period
 
-
     def servers_avalability_check(self):
         lock = self.available_servers_lock
+        self.available_servers = [] 
 
         while True:
             if lock.acquire(blocking=True):
                 available_servers = set()
                 check_threads = []
-                return_values = [None] * len(self.all_servers)
+                server_availability_result = [None] * len(self.all_servers)
 
                 try:
                     for id, server in enumerate(self.all_servers):
-                        check_threads.append(threading.Thread(target=self.server_check, name=f"check_t{id}", args=(server,)))
+                        check_threads.append(threading.Thread(target=self.server_check, name=f"check_t{id}", args=(server_availability_result, id, server,)))
                         check_threads[-1].start()
 
                     for id, server in enumerate(self.all_servers):
                         check_threads[id].join()
 
                     for id, server in enumerate(self.all_servers):
-                        if return_values[id]:
+                        if server_availability_result[id]:
                             available_servers.add(self.all_servers[id])
                     self.available_servers = list(available_servers)
                     lock.release()
@@ -43,27 +56,23 @@ class LoadBalancer:
                     print("An error occurred:", e)
                     lock.release()
 
-    
-    def server_check(self, server) -> bool:
+    def server_check(self, server_availability_result, server_id, server):
         server_ip, server_port = server 
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             try:
-                s.connect((server_ip, server_port))
-                s.sendall(b"connection_check")
+                s.connect((server_ip, int(server_port)))
+                data = RequestType.CONNECTION_TEST.value.encode()
+                s.sendall(data)
                 data = s.recv(1024).decode().strip()
-                if data == "Server is alive":
-                    return True
-                return False 
+                
+                if data == ResponeType.OK.value:
+                    server_availability_result[server_id] = True
+                    return 
+                server_availability_result[server_id] = False
+                return 
             except socket.error:
-                return False
-            
-
-    def connect_to_server(self, host, port, data):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((host, port))
-            s.sendall(b"Hello, world")
-            data = s.recv(1024)
-        print(f"Received {data!r}")
+                server_availability_result[server_id] = False
+                return 
 
     def get_response(self):
         server_index = 0 
@@ -79,8 +88,8 @@ class LoadBalancer:
                     prev_server_index = server_index
                     for server_index in [*range(prev_server_index, len(self.available_servers)), *range(0, prev_server_index)]:
                         candidate_server = self.available_servers[server_index]
-                        if self.send_request(candidate_server, item):
-                            print(f"request with item={item} is performed by server={candidate_server}")
+                        if self.send_request_to_server(candidate_server, item):
+                            print(f"request with num={item} is performed by server={candidate_server}")
                             break 
                     else:
                          print(f"all servers are down and request with item={item} is lost")
@@ -89,23 +98,57 @@ class LoadBalancer:
                     break 
             self.request_queue.task_done()
 
-    def send_request(self, server, item):
+    def send_request_to_server(self, server, item):
         server_ip , server_port = server 
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-                s.connect((server_ip, server_ip))
+                s.connect((server_ip, int(server_port)))
                 data = f"{item}"
                 encoded_data = data.encode()
                 s.sendall(encoded_data)
                 data = s.recv(1024)
-                if data.decode() == "OK":
-                    s.close()
+                if data.decode().strip() == ResponeType.OK.value:
                     return True 
-                else:
-                    return False
+                return False
         except (socket.timeout, ConnectionRefusedError, ConnectionError) as e:
-            # print(f"Error: {e}")
             return False
+
+    def extract_servers(self, config_path):
+        extracted_servers = set()
+        pattern = r"\[(\d+\.\d+\.\d+\.\d+)\]\s*\[(\d+)\]"
+
+        with open(config_path, 'r') as f:
+            for line in f:
+                if line.strip()[0] != '[':
+                    continue 
+                match = re.search(pattern, line.strip())
+                ip, port = match.group(1), match.group(2)
+                extracted_servers.add((ip, port))
+            return list(extracted_servers)
+
+    def handle_client(self, conn, addr):
+        print(f"Connected by {addr}")
+        try:
+            with conn:
+                while True:
+                    data = conn.recv(1024).decode().rstrip()
+                    try:    
+                        num = int(data)
+                        self.request_queue.put(num)
+                        conn.sendall(f"{ResponeType.OK.value}\n".encode())
+                    except Exception as e:
+                        conn.sendall(f"{ResponeType.INVALID_REQUEST.value}\n".encode())
+        except Exception as e:
+            print(f'connection with {addr} closed')
+
+    def get_request(self):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+            s.bind((self.ip, int(self.port)))
+            s.listen()
+            print(f"load balancer listening on ({self.ip} , {self.port})")
+            while True:
+                conn, addr = s.accept()
+                threading.Thread(target=self.handle_client, args=(conn, addr)).start()
 
 
     def start(self):
@@ -116,28 +159,11 @@ class LoadBalancer:
         get_respone_thread = threading.Thread(target=self.get_response, name="server_get_respone")
         get_respone_thread.start()
 
-        self.request_queue = Queue()
-        self.request_queue.ta
-        while True:
-            num = input().rstrip()
-            try:
-                num = int(num)
-            except Exception as e:
-                print("invalid input. you must send only one number to get your response.")
-            self.request_queue.put(num)
+        get_request_thread = threading.Thread(target=self.get_request, name="server_get_request")
+        get_request_thread.start()
 
-    def extract_servers(self, config_path):
-        extracted_servers = set()
-        pattern = r"\[(\d+\.\d+\.\d+\.\d+)\]\s*\[(\d+)\]"
-
-        with open(config_path, 'r') as f:
-            for line in f:
-                match = re.search(pattern, line.rstrip())
-                ip, port = match.group(1), match.group(2)
-                extracted_servers.add((ip, port))
-            return list(extracted_servers)
-
-    
+ 
 
 # connect_to_server(HOST, PORT)
-LoadBalancer(config_path).start()
+load_balancer = LoadBalancer(ip, port, config_path)
+load_balancer.start()
--- HW2/server.py
+++ HW2/server.py
@@ -1,14 +1,22 @@
 import socket
 import sys
+from enum import Enum
 
 HOST = sys.argv[1]  
 PORT = sys.argv[2]  
+
+class RequestType(Enum):
+    CONNECTION_TEST = 'CONNECTION_TEST'
+
+class ResponeType(Enum):
+    OK = 'OK'
+    INVALID_REQUEST = "INVALID_REQUEST"
 
 print(f"server={(HOST, PORT)} is up now.")
 
 def connect_to_load_balancer(host, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.bind((host, port))
+        s.bind((host, int(port)))
         s.listen()
         while True:
             conn, addr = s.accept()
@@ -17,15 +25,16 @@ def connect_to_load_balancer(host, port):
                     data = conn.recv(1024).decode().strip()
                     if not data:
                         break
-                    elif data == "connection_check":
+                    elif data == RequestType.CONNECTION_TEST.value:
                         print("Received connection check from client")
-                        conn.sendall(b"Server is alive")
+                        conn.sendall(ResponeType.OK.value.encode())
                     else:
                         try:
                             item = int(data)
                             print(f"It's instance number {item}")
-                            conn.sendall(b"OK")
+                            conn.sendall(ResponeType.OK.value.encode())
                         except ValueError:
+                            conn.sendall(ResponeType.INVALID_REQUEST.value.encode())
                             print("Invalid data received from client:", data)
 
 connect_to_load_balancer(HOST, PORT)
